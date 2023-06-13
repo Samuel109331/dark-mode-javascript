88f4f420f2eec78795c7c475b7307043ba66fcc7
Samuel Getachew <60266765+Samuel109331@users.noreply.github.com>
2023-06-13 23:50:09 +0530

    Create dark.js

---
+++ dark.js
@@ -0,0 +1,12 @@
+var i = 0;
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'd') {
+    i++
+    if (i%2 != 0) {
+    document.body.style.backgroundColor = "#000";
+    }
+    else {
+    document.body.style.backgroundColor = "#000";
+    }
+  }
+});
