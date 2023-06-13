c3c29917e749450da5265d490af4230c5ae592a7
Samuel Getachew <60266765+Samuel109331@users.noreply.github.com>
2023-06-13 23:59:56 +0530

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,12 +1,11 @@
 var i = 0;
 document.addEventListener('keydown', function(event) {
   if (event.key === 'd') {
-    i++
-    if (i%2 != 0) {
-    document.body.style.backgroundColor = "#000";
-    }
-    else {
-    document.body.style.backgroundColor = "#000";
+    i++;
+    if (i % 2 !== 0) {
+      document.body.style.backgroundColor = "#000";
+    } else {
+      document.body.style.backgroundColor = "#fff"; // Change this to the desired color
     }
   }
 });
